97f09e7c7ceef767f08a9da2da2a1dba92ef9a9f
Gabriela Gutierrez <gabi_guti_1@hotmail.com>
2019-10-20 15:09:08 -0300

    Basic react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h2>IHC Articles</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
