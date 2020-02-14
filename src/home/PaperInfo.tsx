ba99c47adb9a56b0092608d7630835e5ba1d709c
Gabriela Gutierrez <gabi_guti_1@hotmail.com>
2020-02-14 01:05:22 -0300

    Format/style article info card

--- src/home/PaperInfo.tsx
+++ src/home/PaperInfo.tsx
@@ -1,5 +1,4 @@
-import { Typography } from '@material-ui/core'
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import { Typography, Link, Card, CardHeader, CardContent } from '@material-ui/core'
 import Skeleton from '@material-ui/lab/Skeleton'
 import React, { useEffect, useState } from 'react'
 import { PaperInfoResponse, PaperRefsResponse } from '../Constants'
@@ -13,8 +12,6 @@ interface CustomProps {
 const PaperInfo = (props: CustomProps) => {
 
   const service = ServiceWorker.getInstance()
-
-  const emptyMessage = 'Nothing to show'
 
   const [loading, setLoading] = useState(false)
   const [paperInfo, setPaperInfo]: any = useState<PaperInfoResponse>({} as PaperInfoResponse)
@@ -39,84 +36,113 @@ const PaperInfo = (props: CustomProps) => {
   }, [props.references])
 
   return (
-    <div className="flex flex-column justify-center ma6">
-      {(props.id == undefined || loading) &&
-        <>
-          {/* Title */}
-          <div className="flex-auto w-50 pt2 pb4"><Skeleton variant="text" animation={loading ? 'wave' : false} height={35} /></div>
-          <div className="flex flex-row pb5">
-            <div className="flex-auto flex-column w-80">
-              {/* Abstract */}
-              <div className="flex-auto w-20"><Skeleton variant="text" animation={loading ? 'wave' : false} height={30} /></div>
-              <div className="flex-auto w-80"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
-              <div className="flex-auto w-80"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
-              <div className="flex-auto w-80"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
-              <div className="flex-auto w-70"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
-            </div>
-            <div className="flex-auto flex-column w-20">
-              {/* Authors */}
-              <div className="flex-auto w-60"><Skeleton variant="text" animation={loading ? 'wave' : false} height={30} /></div>
-              <div className="flex-auto w-90"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
-              <div className="flex-auto w-80"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
-              <div className="flex-auto"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
-            </div>
-          </div>
-          {/* Cited */}
-          <div className="flex-auto w-20"><Skeleton variant="text" animation={loading ? 'wave' : false} height={30} /></div>
-          <div className="flex-auto w-40"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
-          <div className="flex-auto w-30"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
-          <div className="flex-auto w-40 pb4"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
-          {/* Cited By */}
-          <div className="flex-auto w-20"><Skeleton variant="text" animation={loading ? 'wave' : false} height={30} /></div>
-          <div className="flex-auto w-30"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
-          <div className="flex-auto w-40"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
-        </>
-      }
-      {!(props.id == undefined) && !loading && paperInfo && paperReferences
-        && (
+    <Card className="flex flex-column justify-center ma5 pa4">
+      <CardHeader
+        title="Artigo"
+        titleTypographyProps={{
+          color: "textSecondary",
+          variant: "overline"
+        }}
+      />
+      <CardContent>
+        {
+          props.id == undefined &&
+          <Typography variant="subtitle1" color="textSecondary">Nenhum artigo selecionado</Typography>
+        }
+        {loading &&
           <>
-            <div className="flex-auto pt2 pb4">
-              <Typography variant="h5" gutterBottom>{paperInfo.title}</Typography>
-            </div>
+            {/* Title */}
+            <div className="flex-auto w-50 pt2 pb4"><Skeleton variant="text" animation={loading ? 'wave' : false} height={35} /></div>
             <div className="flex flex-row pb5">
-              <div className="flex-auto flex-column w-80 pr5">
-                <Typography variant="body1" gutterBottom>{paperInfo.abstract_PT}</Typography>
+              <div className="flex-auto flex-column w-80">
+                {/* Abstract */}
+                <div className="flex-auto w-20"><Skeleton variant="text" animation={loading ? 'wave' : false} height={30} /></div>
+                <div className="flex-auto w-80"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
+                <div className="flex-auto w-80"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
+                <div className="flex-auto w-80"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
+                <div className="flex-auto w-70"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
               </div>
               <div className="flex-auto flex-column w-20">
-                <Typography variant="button" display="block" gutterBottom>
-                  {paperInfo.authors
-                    ? paperInfo.authors.map((author: string) => <span className="flex pb2">{author}</span>)
-                    : null
-                  }
-                </Typography>
+                {/* Authors */}
+                <div className="flex-auto w-60"><Skeleton variant="text" animation={loading ? 'wave' : false} height={30} /></div>
+                <div className="flex-auto w-90"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
+                <div className="flex-auto w-80"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
+                <div className="flex-auto"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
               </div>
             </div>
-            <div className="flex flex-column pb5 w-80">
-              <Typography variant="body1" gutterBottom>
-                {paperReferences && paperReferences.cited && paperReferences.cited.map(([paper_id, paper_title]: [number, string]) =>
-                  <span className="flex pb2">
-                    <a onClick={() => console.log(paper_id)}>
-                      {paper_title}
-                    </a>
-                  </span>
-                )}
-              </Typography>
-            </div>
-            <div className="flex pb3 w-80">
-              <Typography variant="body1" gutterBottom>
-                {paperReferences && paperReferences.citedBy && paperReferences.citedBy.map(([paper_id, paper_title]: [number, string]) =>
-                  <span className="flex pb2">
-                    <a onClick={() => console.log(paper_id)}>
-                      {paper_title}
-                    </a>
-                  </span>
-                )}
-              </Typography>
-            </div>
+            {/* Cited */}
+            <div className="flex-auto w-20"><Skeleton variant="text" animation={loading ? 'wave' : false} height={30} /></div>
+            <div className="flex-auto w-40"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
+            <div className="flex-auto w-30"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
+            <div className="flex-auto w-40 pb4"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
+            {/* Cited By */}
+            <div className="flex-auto w-20"><Skeleton variant="text" animation={loading ? 'wave' : false} height={30} /></div>
+            <div className="flex-auto w-30"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
+            <div className="flex-auto w-40"><Skeleton variant="text" animation={loading ? 'wave' : false} /></div>
           </>
-        )
-      }
-    </div>
+        }
+        {!(props.id == undefined) && !loading && paperInfo && paperReferences
+          && (
+            <>
+              <div className="flex-auto pt2 pb4">
+                <Typography variant="h4" gutterBottom>{paperInfo.title}</Typography>
+              </div>
+              <div className="flex flex-row pb3">
+                <div className="flex-auto flex-column w-80 pr5">
+                  <Typography variant="h6" gutterBottom>Resumo</Typography>
+                  {paperInfo.abstract_PT
+                    ? <Typography variant="body1" gutterBottom>{paperInfo.abstract_PT}</Typography>
+                    : <Typography variant="body1" color="textSecondary" gutterBottom>Nenhum</Typography>
+                  }
+                </div>
+                <div className="flex-auto flex-column w-20">
+                  <Typography variant="h6" gutterBottom>Autores</Typography>
+                  {paperInfo.authors
+                    && paperInfo.authors.length
+                    ? <Typography variant="body1" display="block" gutterBottom>
+                      {paperInfo.authors.map((author: string) => <span className="flex pb2">{author}</span>)}
+                    </Typography>
+                    : <Typography variant="body1" color="textSecondary" gutterBottom>Nenhum</Typography>
+                  }
+                </div>
+              </div>
+              <div className="flex flex-column pb3 w-80">
+                <Typography variant="h6" gutterBottom>Artigos referenciados</Typography>
+                {paperReferences.cited
+                  && paperReferences.cited.length
+                  ? <Typography variant="body1" gutterBottom>
+                    {paperReferences.cited.map(([paper_id, paper_title]: [number, string]) =>
+                      <span key={paper_id} className="flex pb2">
+                        <Link href={`#${paper_id}`} onClick={() => console.log(paper_id)}>
+                          {paper_title}
+                        </Link>
+                      </span>
+                    )}
+                  </Typography>
+                  : <Typography variant="body1" color="textSecondary" gutterBottom>Nenhum</Typography>
+                }
+              </div>
+              <div className="flex flex-column pb3 w-80">
+                <Typography variant="h6" gutterBottom>Referência em</Typography>
+                {paperReferences.citedBy
+                  && paperReferences.citedBy.length
+                  ? <Typography variant="body1" gutterBottom>
+                    {paperReferences.citedBy.map(([paper_id, paper_title]: [number, string]) =>
+                      <span key={paper_id} className="flex pb2">
+                        <Link href={`#${paper_id}`} onClick={() => console.log(paper_id)}>
+                          {paper_title}
+                        </Link>
+                      </span>
+                    )}
+                  </Typography>
+                  : <Typography variant="body1" color="textSecondary" gutterBottom>Nenhum</Typography>
+                }
+              </div>
+            </>
+          )
+        }
+      </CardContent>
+    </Card>
   )
 }
 
