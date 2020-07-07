1aa1a736a57c4b588070c1664a217611332fc81b
Gabriela Gutierrez <gabi_guti_1@hotmail.com>
2020-07-07 13:37:43 -0300

    new deploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const DEV_HOST = 'http://localhost:9100/graphql'
-const LIVE_HOST = 'https://ad79328ea9d6.ngrok.io/graphql'
+const LIVE_HOST = 'https://1f08c551fc40.ngrok.io/graphql'
 
 const httpLink = createHttpLink({
   uri: LIVE_HOST,
