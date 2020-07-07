c55bc827c39e7df65b014e68f24320b896aed9c8
Gabriela Gutierrez <gabi_guti_1@hotmail.com>
2020-07-07 13:38:21 -0300

    Updates

--- precache-manifest.4100e385bb0537860794cb0286603d42.js
+++ precache-manifest.4100e385bb0537860794cb0286603d42.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6966a9c44853e5c919735d71f381ba38",
+    "revision": "5e911bb91315d49d738c736453c4b66b",
     "url": "/papers-ihc-interface/index.html"
   },
   {
-    "revision": "fd81aed136414d79a00c",
+    "revision": "34733bae6a78a6a76bee",
     "url": "/papers-ihc-interface/static/css/2.5a2f7233.chunk.css"
   },
   {
-    "revision": "fd81aed136414d79a00c",
-    "url": "/papers-ihc-interface/static/js/2.70cf0b6d.chunk.js"
+    "revision": "34733bae6a78a6a76bee",
+    "url": "/papers-ihc-interface/static/js/2.dd610289.chunk.js"
   },
   {
     "revision": "1937a0fc6634269e5f47f8181e27fdc7",
-    "url": "/papers-ihc-interface/static/js/2.70cf0b6d.chunk.js.LICENSE.txt"
+    "url": "/papers-ihc-interface/static/js/2.dd610289.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d32fee49cb7e1e57487a",
-    "url": "/papers-ihc-interface/static/js/main.df6eb21f.chunk.js"
+    "revision": "a551c27afcd4f286748b",
+    "url": "/papers-ihc-interface/static/js/main.d64fe815.chunk.js"
   },
   {
     "revision": "6f77b915a5b72cd47c03",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/papers-ihc-interface/precache-manifest.3260e5cd77b970da32cd6eec687a3c03.js"
+  "/papers-ihc-interface/precache-manifest.4100e385bb0537860794cb0286603d42.js"
 );
 
 self.addEventListener('message', (event) => {
