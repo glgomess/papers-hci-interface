ddbf0140e8bf7458e6d70fc54bbf07f1e577e1de
Guilherme Lacerda <lacerdagomes95@gmail.com>
2021-08-29 19:27:33 -0300

    POST request to check login

--- src/pages/login/hooks/useLogin.tsx
+++ src/pages/login/hooks/useLogin.tsx
@@ -1,6 +1,19 @@
+import { AxiosRequestConfig } from 'axios'
 import React from 'react'
-
+import { Backend } from '../../../service/backend'
+import axios from 'axios'
 export function useLogin() {
-  async function login(username: string, password: string) {}
+  async function login(username: string, password: string) {
+    try {
+      const data: AxiosRequestConfig = {
+        data: { username, password },
+      }
+      // console.log('#####', Backend().)
+      await axios.post('http://localhost:5000/login', data)
+    } catch (e) {
+      console.log(e)
+      throw e
+    }
+  }
   return { login }
 }
